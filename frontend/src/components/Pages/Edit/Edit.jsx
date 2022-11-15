ee2afe1d5c7f8d2075dd7b420152cb86f1f51cb7
EliteHacker228 <mchappycraft@yandex.ru>
2022-11-15 15:33:06 +0500

    Add functional to edit page

--- frontend/src/components/Pages/Edit/Edit.jsx
+++ frontend/src/components/Pages/Edit/Edit.jsx
@@ -5,8 +5,108 @@ import r2 from './Rectangle 52.png';
 import r3 from './Rectangle 53.png';
 import r4 from './Rectangle 54.png';
 import {Link} from "react-router-dom";
+import Xarrow, {Xwrapper} from "react-xarrows";
+import {useId, useState} from "react";
 
 const Edit = () => {
+
+    const action = {
+        default: {
+            type: 'default',
+            arrowColor: null
+        },
+        connect: {
+            type: 'connect',
+            arrowColor: 'gray'
+        },
+        split: {
+            type: 'split',
+            arrowColor: 'red'
+        },
+        merge: {
+            type: 'merge',
+            arrowColor: 'blue'
+        },
+        delete: {
+            type: 'delete',
+            arrowColor: null
+        }
+    };
+    const status = {
+        default: 'default',
+        pending: 'pending'
+    };
+
+    const [currentAction, setCurrentAction] = useState(action.default);
+    const [currentStatus, setCurrentStatus] = useState(status.default);
+    const [pendingBlockId, setPendingBlockId] = useState('');
+    const [clickedButton, setClickedButton] = useState('');
+
+    const [arrows, setArrows] = useState([]);
+
+    const structBlockOutArrowHandler = (event) => {
+        console.log(event.target.id);
+        let target = event.target;
+        console.log(currentStatus);
+        if (currentAction.type !== action.default.type) {
+            if (currentStatus === status.default) {
+                setPendingBlockId(target.id);
+                setCurrentStatus(status.pending);
+            }
+            console.log(arrows);
+        }
+    };
+
+    const structBlockInArrowHandler = (event) => {
+        console.log(event.target.id);
+        let target = event.target;
+        console.log(currentStatus);
+        if (currentAction.type !== action.default.type) {
+            if (currentStatus === status.pending) {
+                let startId = pendingBlockId;
+                let endId = target.id;
+                // let arrow = <Xarrow key={startId+endId} start={startId} end={endId}/>;
+                arrows.push([startId, endId, currentAction.arrowColor]);
+                setArrows(arrows);
+                setPendingBlockId('');
+                setCurrentStatus(status.default);
+            }
+            console.log(arrows);
+        }
+    };
+
+    const connectOnClick = (event) => {
+        event.preventDefault();
+        switchActions(action.connect);
+    };
+    const splitOnClick = (event) => {
+        event.preventDefault();
+        switchActions(action.split);
+    };
+    const mergeOnClick = (event) => {
+        event.preventDefault();
+        switchActions(action.merge);
+    };
+    const deleteOnClick = (event) => {
+        event.preventDefault();
+        switchActions(action.delete);
+    };
+
+    const switchActions = (buttonAction) => {
+        console.log(currentAction.type);
+        if(currentAction.type === buttonAction.type) {
+            console.log(action.default.type);
+            setCurrentAction(action.default);
+            setClickedButton(action.default.type)
+        }
+        else{
+            console.log(buttonAction.type);
+            setCurrentAction(buttonAction);
+            setClickedButton(buttonAction.type)
+        }
+    };
+
+
     return (
         <div className={css.page}>
             <div className={css.header}>
@@ -19,49 +119,61 @@ const Edit = () => {
             </div>
             <div className={css.edit}>
                 <div className={css.scheme_board}>
-                    <div className={css.file_struct}>
-                        <h1 className={css.struct_header}>Исходная структура</h1>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='ФИО'/>
+                    <Xwrapper>
+                        <div className={css.file_struct}>
+                            <h1 className={css.struct_header}>Исходная структура</h1>
+                            <div className={css.struct_block} id={'init-' + useId()} onClick={structBlockOutArrowHandler}>
+                                <input className={css.struct_input} type='text' value='ФИО'/>
+                            </div>
+                            <div className={css.struct_block} id={'init-' + useId()} onClick={structBlockOutArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Дата рождения'/>
+                            </div>
+                            <div className={css.struct_block} id={'init-' + useId()} onClick={structBlockOutArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Возраст'/>
+                            </div>
+                            <div className={css.struct_block} id={'init-' + useId()} onClick={structBlockOutArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Диагноз (название)'/>
+                            </div>
+                            <div className={css.struct_block} id={'init-' + useId()} onClick={structBlockOutArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Диагноз (код)'/>
+                            </div>
                         </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Дата рождения'/>
+                        <div className={css.file_struct}>
+                            <h1 className={css.struct_header}>Необходимая структура</h1>
+                            <div className={css.struct_block} id={'wanted-' + useId()}
+                                 onClick={structBlockInArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Фамилия'/>
+                            </div>
+                            <div className={css.struct_block} id={'wanted-' + useId()}
+                                 onClick={structBlockInArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Имя'/>
+                            </div>
+                            <div className={css.struct_block} id={'wanted-' + useId()}
+                                 onClick={structBlockInArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Отчество'/>
+                            </div>
+                            <div className={css.struct_block} id={'wanted-' + useId()}
+                                 onClick={structBlockInArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Возраст'/>
+                            </div>
+                            <div className={css.struct_block} id={'wanted-' + useId()}
+                                 onClick={structBlockInArrowHandler}>
+                                <input className={css.struct_input} type='text' value='Диагноз'/>
+                            </div>
                         </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Возраст'/>
-                        </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Адрес прописки пациента'/>
-                        </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Диагноз (код)'/>
-                        </div>
-                    </div>
-                    <div className={css.file_struct}>
-                        <h1 className={css.struct_header}>Необходимая структура</h1>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='ФИО'/>
-                        </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Дата рождения'/>
-                        </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Возраст'/>
-                        </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Адрес прописки пациента'/>
-                        </div>
-                        <div className={css.struct_block}>
-                            <input className={css.struct_input} type='text' value='Диагноз (код)'/>
-                        </div>
-                    </div>
+                        {arrows.map(startAndEnd => <Xarrow kay={startAndEnd[0] + startAndEnd[1]} start={startAndEnd[0]}
+                                                           end={startAndEnd[1]}
+                                                           color={startAndEnd[2]}
+                                                           strokeWidth='4'
+                                                           headSize='4'/>)}
+                    </Xwrapper>
                 </div>
                 <div className={css.control_panel}>
                     <h1 className={css.control_panel_header}>Инструменты</h1>
                     <ul className={css.controls}>
                         <li className={css.control}>
-                            <button className={css.control_button}>
-                                <img src={r0} className={css.control_button_img}/>
+                            <button className={css.control_button} onClick={connectOnClick}>
+                                <img src={r0} className={clickedButton === 'connect'? css.control_button_active : css.control_button_img}/>
                             </button>
                             <div>
                                 <h2 className={css.control_name}>Связь между столбцами</h2>
@@ -70,8 +182,8 @@ const Edit = () => {
                             </div>
                         </li>
                         <li className={css.control}>
-                            <button className={css.control_button}>
-                                <img src={r2}/>
+                            <button className={css.control_button} onClick={splitOnClick}>
+                                <img src={r2} className={clickedButton === 'split'? css.control_button_active : css.control_button_img}/>
                             </button>
                             <div>
                                 <h2 className={css.control_name}>Разделение</h2>
@@ -80,8 +192,8 @@ const Edit = () => {
                             </div>
                         </li>
                         <li className={css.control}>
-                            <button className={css.control_button}>
-                                <img src={r3}/>
+                            <button className={css.control_button} onClick={mergeOnClick}>
+                                <img src={r3} className={clickedButton === 'merge'? css.control_button_active : css.control_button_img}/>
                             </button>
                             <div>
                                 <h2 className={css.control_name}>Слияние</h2>
@@ -90,8 +202,8 @@ const Edit = () => {
                             </div>
                         </li>
                         <li className={css.control}>
-                            <button className={css.control_button}>
-                                <img src={r4} className={css.control_button_img}/>
+                            <button className={css.control_button} onClick={deleteOnClick}>
+                                <img src={r4} className={clickedButton === 'delete'? css.control_button_active : css.control_button_img}/>
                             </button>
                             <div>
                                 <h2 className={css.control_name}>Удалить</h2>
